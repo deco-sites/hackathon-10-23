07d5d53d76793dcea4d291adb307eb41c752edfc
Igor Brasileiro <brasileiro456@gmail.com>
2023-10-28 00:57:34 -0300

    Change color

--- components/ui/HeroAndHeader.tsx
+++ components/ui/HeroAndHeader.tsx
@@ -74,7 +74,7 @@ export default function HeroAndHeader(
                       class="relative flex justify-center text-[#00FF80]"
                       href={cta?.href}
                     >
-                      <button class="relative w-[500px] h-[88px] bg-[#949E9E] hover:bg-[#FFF] flex justify-center items-center">
+                      <button class="relative w-[500px] h-[88px] bg-[#949E9E] hover:bg-[#949E9E] flex justify-center items-center">
                         <div class="triangulo-top-right-mini md:triangulo-top-right absolute top-[-1px] right-[-1px]" />
                         <div class="triangulo-top-left-mini md:triangulo-top-left absolute top-[-1px] left-[-2px]" />
                         <div class="triangulo-bottom-right-mini md:triangulo-bottom-right absolute bottom-[-1px] right-[-1px]" />
